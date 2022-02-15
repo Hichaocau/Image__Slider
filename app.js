def6b785ddf574603c75eaa3497b86ef0a8b5fb5
Dat <91059288+Hichaocau@users.noreply.github.com>
2022-02-15 16:02:04 +0700

    up

---
+++ app.js
@@ -0,0 +1,44 @@
+let i = 0; //current slide
+let j = 5; //total slide
+
+const btnNext = document.querySelector('#next')
+const btnPrev = document.querySelector('#prev')
+const dots = document.querySelectorAll('.dot-container button')
+const images = document.querySelectorAll('.image-container img')
+
+function next(){
+    document.getElementById('content' + (i+1)).classList.remove("active"); 
+    i = ( j + i + 1) % j; //lấy số dư
+    console.log(i)
+    document.getElementById('content' + (i+1)).classList.add("active"); 
+    dotsActive(i + 1)
+}
+
+function prev(){
+    document.getElementById('content' + (i+1)).classList.remove("active"); 
+    i = ( j + i - 1) % j; //lấy số dư
+    console.log(i)
+    document.getElementById('content' + (i+1)).classList.add("active"); 
+    dotsActive(i + 1)
+}
+
+// change bgc dots
+function dotsActive(num){
+    dots.forEach(function(dot){
+        dot.style.background = 'transparent'
+    });
+    document.querySelector('.dot-container button:nth-child('+ num +')').style.background = '#8052ec'
+}
+
+btnNext.addEventListener('click', next)
+btnPrev.addEventListener('click', prev)
+
+// dot click
+function dot(index){
+    images.forEach(function(img){
+        img.classList.remove('active')
+    })
+    document.getElementById('content' + index).classList.add("active"); 
+    i = index -1 
+    dotsActive(index)
+}
